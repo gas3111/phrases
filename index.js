b2637b2cc26589b64d2ceacd12350019449567e0
gas3111 <47702105+gas3111@users.noreply.github.com>
2019-02-16 22:28:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 phrases = [
-    "hello",
-    "world",
-    "new"
+"Airbnb is the poster child of the 'sharing economy.' (a person or thing that is seen as a typical example of something)",
+"Jack is the poster child for wasted talent. (a person or thing that is seen as a typical example of something)",
+"More money is needed to get this project off the ground. (to begin to operate or proceed in a successful way)"
 ]
 
 function displayPhrase() {
